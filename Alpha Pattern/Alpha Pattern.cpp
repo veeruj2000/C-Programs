8a56a694df6baf28becfc6de2f17f93b4730c368
UJWAL PANKAJ JAIN <57255611+veeruj2000@users.noreply.github.com>
2021-11-04 19:07:21 +0530

    Alpha Pattern
    Prints alphabets till numbers given in input with shaping the wall of the right angle triangle.

---
+++ Alpha Pattern/Alpha Pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter Numbers"<< endl;
+	cin>> n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		char ch= 'A'+i-1;
+		while(j<=i){	
+			cout<<ch;
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
